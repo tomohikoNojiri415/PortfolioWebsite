2bd2195d86f66ff2cf176969dd2c923a99542e2e
TomohikoNojiri <nojiri415@gmail.com>
2024-05-07 00:14:32 +1000

    added modal to certificates

--- resources/helper.js
+++ resources/helper.js
@@ -1,10 +1,10 @@
 function toggleMobileMenu() {
-  var mobileNav = document.getElementById('mobileNav');
+  const mobileNav = document.getElementById('mobileNav');
   mobileNav.classList.toggle('show');
 }
 
 const closeMobileMenu = () => {
-  var mobileNav = document.getElementById('mobileNav');
+  const mobileNav = document.getElementById('mobileNav');
   mobileNav.classList.remove('show');
 }
 
@@ -21,78 +21,35 @@ const mobileScrollToSection = (sectionId) => {
   scrollToSection(sectionId);
 }
 
-// document.addEventListener('DOMContentLoaded', () => {
-//   const certificateIcons = document.querySelectorAll('.certificate-icon');
+//close modal
+const closeModal = () => {
+  const modal = document.getElementById("certificateModal");
+  modal.style.display = 'none';
+}
 
-//   const startBounce = (icon) => {
-//       icon.classList.add('bounce2');
-//       setTimeout(() => {
-//           icon.classList.remove('bounce2');
-//       }, 1500);
-//   }
-//   certificateIcons.forEach((icon, index) => {
-//       setTimeout(() => {
-//           startBounce(icon);
-//       }, index * 500); // Adjust the delay between each icon's bounce as needed
-//   });
-// });
 document.addEventListener('DOMContentLoaded', () => {
   const certificateIcons = document.querySelectorAll('.certificate-icon');
-
+  const modal = document.getElementById("certificateModal");
+  //certificate icon bounce effect
   const startBounce = (icon) => {
     icon.classList.add('bounce2');
   }
 
   certificateIcons.forEach((icon, index) => {
-      setTimeout(() => {
-          startBounce(icon);
-      }, index * 500); // Adjust the delay between each icon's bounce as needed
+    setTimeout(() => {
+        startBounce(icon);
+    }, index * 500); 
+  });
+  //modal open
+  certificateIcons.forEach(icon => {
+    icon.addEventListener('click', () => {
+      const img = modal.querySelector('img'); 
+      const imageName = icon.getAttribute('data-image-name');
+      img.src = `./resources/media/${imageName}.png`; 
+
+      // Display the corresponding modal
+      modal.style.display = 'block';
+    });
   });
 });
 
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const certificateIcons = document.querySelectorAll('.certificate-icon');
-//   let jumpEffectApplied = false;
-
-//   const jumpUpIcons = () => {
-//       console.log('jumpUpIcons');
-//       certificateIcons.forEach(icon => {
-//           // icon.style.transform = 'translateY(-4px)'; 
-//         icon.classList.add('jump');
-//       });
-//   }
-
-//   const resetIconsPosition = () => {
-//       certificateIcons.forEach(icon => {
-//           icon.style.transform = 'none';
-//       });
-//   }
-//   window.addEventListener('scroll', () => {
-//       // const certificateSection = document.getElementById('certificate-section');
-//       // const certificateSectionTop = certificateSection.offsetTop;
-//       // const certificateSectionHeight = certificateSection.clientHeight;
-//       // const scrollPosition = window.scrollY;
-//       // console.log(certificateSectionTop, certificateSectionHeight, scrollPosition);
-
-//       // if (scrollPosition >= certificateSectionTop && scrollPosition < certificateSectionTop + certificateSectionHeight) {
-//       //     jumpUpIcons();
-//       // } else {
-//       //     resetIconsPosition();
-//       // }
-//     const certificateSection = document.getElementById('certificate-section');  
-//     var offsetHeight = certificateSection.offsetHeight;
-//     const offsetTop = certificateSection.offsetTop;
-//     const scrollPosition = window.scrollY;
-//     // console.log(offsetTop, offsetHeight, scrollPosition);
-//     if ( scrollPosition < offsetTop + 50 && scrollPosition > offsetTop-offsetHeight - 200){
-//       if (jumpEffectApplied) return;
-//       jumpEffectApplied = true;
-//       jumpUpIcons();
-//     }else{
-//       if (!jumpEffectApplied) return;
-//       // console.log('not in the middle');
-//       jumpEffectApplied = false;
-//     }
-//   });
-// });
